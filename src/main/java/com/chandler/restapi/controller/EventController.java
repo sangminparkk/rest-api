99692d3c45080196469dfa3a651c55a15d2444cb
chandler <parksm0529@gmail.com>
2024-10-29 11:49:36 +0900

    feat) convert response to json

--- src/main/java/com/chandler/restapi/controller/EventController.java
+++ src/main/java/com/chandler/restapi/controller/EventController.java
@@ -1,20 +1,23 @@
 package com.chandler.restapi.controller;
 
+import com.chandler.restapi.domain.Event;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@Controller
+@RestController
 public class EventController {
 
     @PostMapping("/api/events")
-    public ResponseEntity createEvent() {
-        URI createdUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
-        return ResponseEntity.created(createdUri).build();
+    public ResponseEntity createEvent(@RequestBody Event event) throws JsonProcessingException {
+        URI createdUri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri();
+        return ResponseEntity.created(createdUri).body(event);
     }
 }
