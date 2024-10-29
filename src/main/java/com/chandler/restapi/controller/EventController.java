aad25ceeeaa8d8fb9c1a2f597259e6e184910a01
chandler <parksm0529@gmail.com>
2024-10-29 15:46:21 +0900

    refactor) use @RequestMapping to share mapping on at a class level

--- src/main/java/com/chandler/restapi/controller/EventController.java
+++ src/main/java/com/chandler/restapi/controller/EventController.java
@@ -1,23 +1,24 @@
 package com.chandler.restapi.controller;
 
 import com.chandler.restapi.domain.Event;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping(value = "/api/events", produces = HAL_JSON_VALUE)
 public class EventController {
 
-    @PostMapping("/api/events")
-    public ResponseEntity createEvent(@RequestBody Event event) throws JsonProcessingException {
-        URI createdUri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri();
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
         event.setId(1L);
         return ResponseEntity.created(createdUri).body(event);
     }
