7a1593d69f283d0a23c97e584968c1b58bc8ce80
chandler <parksm0529@gmail.com>
2024-10-31 19:02:58 +0900

    refactor) Parameters for a couple of cases

--- src/test/java/com/chandler/restapi/domain/EventTest.java
+++ src/test/java/com/chandler/restapi/domain/EventTest.java
@@ -1,11 +1,17 @@
 package com.chandler.restapi.domain;
 
+import junitparams.JUnitParamsRunner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@RunWith(JUnitParamsRunner.class)
 class EventTest {
 
     @DisplayName("@Builder 적용되었는지")
@@ -36,82 +42,59 @@ class EventTest {
         assertNotNull(event);
     }
 
-    @Test
+//    @CsvSource({
+//            "0, 0, true",
+//            "100, 0, false",
+//            "0, 100, false"
+//    }) // Type safe 하지 않음
+    @ParameterizedTest
+    @MethodSource(value = "parametersForTestFree") // "paramsForTestFree" 를 안쓰고도 적용되는지 체크
     @DisplayName("basePrice와 maxPrice가 둘다 0이면 무료입니다")
-    public void testFree() {
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         //given
-        Event event1 = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
-                .build();
-
-        //when
-        event1.update();
-
-        //then
-        assertThat(event1.isFree()).isTrue();
-
-        //given
-        Event event2 = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        //when
-        event2.update();
-
-        //then
-        assertThat(event2.isFree()).isFalse();
-
-        //given
-        Event event3 = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        //when
-        event3.update();
-
-        //then
-        assertThat(event3.isFree()).isFalse();
-    }
-
-    @Test
-    @DisplayName("location 값이 존재하면 오프라인")
-    public void testOffline() {
-        //given
-        Event event = Event.builder()
-                .location("서울시 강남구")
+        Event event= Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         //when
         event.update();
 
         //then
-        assertThat(event.isOffline()).isTrue();
-
-        //given
-        Event event1 = Event.builder()
-                .location("")
-                .build();
-
-        //when
-        event1.update();
-
-        //then
-        assertThat(event1.isOffline()).isFalse();
-
-        //given
-        Event event2 = Event.builder()
-                .location(null)
-                .build();
-
-        //when
-        event2.update();
-
-        //then
-        assertThat(event2.isOffline()).isFalse();
-
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
+
+    public static Object[] parametersForTestFree() {
+        return new Object[]{
+                new Object[]{0, 0, true},
+                new Object[]{100, 0, false},
+                new Object[]{0, 100, false},
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "parametersForTestOffline")
+    @DisplayName("location 값이 존재하면 오프라인")
+    public void testOffline(String location, boolean isOffline) {
+        //given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    public static Object[] parametersForTestOffline() {
+        return new Object[]{
+                new Object[]{"서울시 강남구", true},
+                new Object[]{"        ", false},
+                new Object[]{null, false},
+        };
+    }
+
 
 }
