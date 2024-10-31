331ea15375dd4aec2137f7472828858142340b86
chandler <parksm0529@gmail.com>
2024-10-31 16:41:08 +0900

    feat) Business logic - Check free and offline

--- src/main/java/com/chandler/restapi/domain/Event.java
+++ src/main/java/com/chandler/restapi/domain/Event.java
@@ -47,4 +47,17 @@ public class Event {
 
     private LocalDateTime closeEventDateTime;
 
+    public void update() {
+        if (this.maxPrice == 0 && this.basePrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+
+        if (this.location == null || this.location.isBlank()) { // empty를 커버한다
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+    }
 }
