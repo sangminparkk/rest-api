708e798cd10dd13966f2aa66449f01e81292e0c4
chandler <parksm0529@gmail.com>
2024-11-08 10:37:28 +0900

    feat) Implements userDetailsService to find username

---
+++ src/main/java/com/chandler/restapi/service/AccountService.java
@@ -0,0 +1,37 @@
+package com.chandler.restapi.service;
+
+import com.chandler.restapi.domain.Account;
+import com.chandler.restapi.domain.AccountRole;
+import com.chandler.restapi.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name()))
+                .collect(Collectors.toSet());
+    }
+}
