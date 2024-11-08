4a77eb89059d4e8cb10da19c446726ae67632b69
chandler <parksm0529@gmail.com>
2024-11-08 10:35:17 +0900

    test) UserDetailsService for checking a username - part of TDD

---
+++ src/test/java/com/chandler/restapi/service/AccountServiceTest.java
@@ -0,0 +1,66 @@
+package com.chandler.restapi.service;
+
+import com.chandler.restapi.domain.Account;
+import com.chandler.restapi.repository.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+import static com.chandler.restapi.domain.AccountRole.ADMIN;
+import static com.chandler.restapi.domain.AccountRole.USER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class AccountServiceTest {
+
+    /**
+     * mock 이 필요하지 않는 이유는, controller 호출할 필요가 없기 때문에
+     */
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    @DisplayName("유저 네임으로 user 확인")
+    public void findByUsername() throws Exception {
+        // given
+        String username = "chandler@gmail.com";
+        String password = "chandler";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(ADMIN, USER))
+                .build();
+        this.accountRepository.save(account);
+
+        // when
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // then
+        assertEquals(userDetails.getPassword(), password);
+    }
+
+
+    @Test
+    @DisplayName("유저 정보가 없는 경우 NotFoundException")
+    public void findByUsername_NotFound() throws Exception {
+        // given : 데이터를 넘길 필요가 없죠
+
+        // when
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+
+        // then
+        assertThrows(UsernameNotFoundException.class, () -> userDetailsService.loadUserByUsername("username"));
+    }
+
+}
