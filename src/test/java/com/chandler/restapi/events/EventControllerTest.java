d93a391f05d5cb0d4936ef06bbcb5a7ef6054ff2
chandler <parksm0529@gmail.com>
2025-02-11 21:51:26 +0900

    feat) 201 response by POST /api/events

--- src/test/java/com/chandler/restapi/events/EventControllerTest.java
+++ src/test/java/com/chandler/restapi/events/EventControllerTest.java
@@ -1,14 +1,19 @@
 package com.chandler.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.hateoas.MediaTypes.HAL_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,12 +23,38 @@ class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
+        var event = getEvent();
+
         mockMvc.perform(post("/api/events")
                         .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(event))
                         .accept(HAL_JSON))
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+        ;
+    }
+
+    private static Event getEvent() {
+        var event = Event.builder()
+                .id(1)
+                .name("Spring")
+                .description("REST API with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2025, 2, 11, 12, 12,12))
+                .closeEnrollmentDateTime(LocalDateTime.of(2025, 2, 13, 12, 12,12))
+                .beginEventDateTime(LocalDateTime.of(2025, 2, 14, 12, 12,12))
+                .endEventDateTime(LocalDateTime.of(2025, 2, 15, 12, 12,12))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("잠실역")
+                .build();
+        return event;
     }
 
 }
